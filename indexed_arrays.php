fb413ccc86e7823031a71ab675f2b6acadc12a1c
Jim Vang <vangjim15@gmail.com>
2017-08-05 20:40:10 -0500

    removing array elements

--- indexed_arrays.php
+++ indexed_arrays.php
@@ -26,3 +26,24 @@ var_dump($learn);
 ?>
 
 <!-- Removing Array Elements -->
+<?php
+$learn = array('Conditionals', 'Arrays', 'Loops');
+// [] tells php to add something to the array
+$learn[] = 'Build something awesome!';
+// push add objects to the end of the array
+array_push($learn, 'Functions', 'Forms', 'Objects');
+// unshift add objects to the beginning of the array
+array_unshift($learn, 'HMTL', 'CSS');
+array_shift($learn);
+
+echo 'you removed ' . array_shift($learn);
+echo 'you removed ' . array_pop($learn);
+unset($learn[1], $learn[2]); // remove any specific array element as long as you know the key. Unset can take multiple parameters. Remove keys one and two and values of conditionals and arrays are gone as well.
+
+// this will remove $learn
+// $learn = array_values($learn);
+// unset($learn)
+
+var_dump($learn);
+
+?>
