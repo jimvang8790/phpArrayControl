fdf3d4d6b324f8b8b3aca14e5acac320227f7701
Jim Vang <vangjim15@gmail.com>
2017-08-10 22:25:42 -0500

    added while list array

--- loops.php
+++ loops.php
@@ -11,3 +11,23 @@ do {
   echo $year . "<br />\n";
 } while (++$year <= $currentYear);
 ?>
+
+<!-- While array list -->
+<?php
+$currentYear = date('Y');
+$year = $currentYear - 100;
+
+$learn = array('Conditionals', 'Arrays', 'Loops');
+$learn[] = 'Build something awesome!';
+array_push($learn, 'Functions', 'Forms', 'Objects');
+array_unshift($learn, 'HMTL', 'CSS');
+array_shift($learn);
+asort($learn); // sort by value and not keys
+
+$count = 0;
+// seeing the first two items in array
+while ((list($key, $val) = each($learn)) && $count++ < 2) {
+  echo "$key => $val\n";
+}
+
+?>
