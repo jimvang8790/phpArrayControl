2e48cac62c0b437cff36c6338644dfd128df90f4
Jim Vang <vangjim15@gmail.com>
2017-08-12 21:47:13 -0500

    added todo app

--- todo.php
+++ todo.php
@@ -57,3 +57,60 @@ echo $list[0]['title'];
 
 echo '</table>';
 ?>
+
+<!-- ToDo App -->
+<?php
+include 'list.php';
+
+$status = 'all';
+$field = 'priority';
+
+$order = array();
+if ($status == 'all') {
+  $order = array_keys($list);
+} else {
+  for ($list as $key => $item) {
+    if ($item['complete'] == $status) {
+      $order [] = $key;
+    }
+  }
+}
+
+if ($field) {
+  $sort = array();
+  foreach ($order as $id) {
+    $sort[id] = $list[$id][$field];
+  }
+  asort($sort);
+  $order = array_keys($sort);
+}
+
+//var_dump($sort);
+////var_dump($list);
+
+echo '<table>';
+echo '<tr>';
+echo '<th>Title</tr>';
+echo '<th>Priority</th>';
+echo '<th>Due Date</th>';
+echo '<th>Complete</th>';
+echo '</tr>';
+
+foreach ($order as $id) {
+  echo '<tr>';
+  echo '<td>' . $list[$id]['title'] . "</td>\n";
+  echo '<td>' . $list[$id]['priority'] . "</td>\n";
+  echo '<td>' . $list[$id]['due'] . "</td>\n";
+  echo '<td>';
+  if ($list[$id]['complete']) {
+    echo 'Yes';
+  } else {
+    echo 'No';
+  }
+  echo "</td>\n";
+  echo '</tr>';
+}
+echo $list[0]['title'];
+
+echo '</table>';
+?>
