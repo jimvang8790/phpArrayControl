d65b2b438baa045a53c93981f5855544984e53b0
Jim Vang <vangjim15@gmail.com>
2017-08-12 21:58:05 -0500

    Feature Switch This Week

--- todo.php
+++ todo.php
@@ -114,3 +114,71 @@ echo $list[0]['title'];
 
 echo '</table>';
 ?>
+
+<!-- Feature Switch This Week -->
+<?php
+include 'list.php';
+
+$status = 'all';
+$field = 'priority';
+$action = 'week';
+
+$order = array();
+if ($status == 'all') {
+  $order = array_keys($list);
+} else {
+  for ($list as $key => $item) {
+    if ($item['complete'] == $status) {
+      $order [] = $key;
+    }
+  }
+}
+
+switch ($action) {
+    case 'sort';
+      if ($field) {
+        $sort = array();
+        foreach ($order as $id) {
+          $sort[id] = $list[$id][$field];
+        }
+        asort($sort);
+        $order = array_keys($sort);
+      }
+      break;
+    case 'week';
+      foreach ($order as $key => $value) {
+        if (strtotime($list[$value]['due']) > strtotime("+1 week") || !$list[$value]['due']) {
+            unset($order[$key]);
+        }
+      }
+ }
+
+//var_dump($sort);
+////var_dump($list);
+
+echo '<table>';
+echo '<tr>';
+echo '<th>Title</tr>';
+echo '<th>Priority</th>';
+echo '<th>Due Date</th>';
+echo '<th>Complete</th>';
+echo '</tr>';
+
+foreach ($order as $id) {
+  echo '<tr>';
+  echo '<td>' . $list[$id]['title'] . "</td>\n";
+  echo '<td>' . $list[$id]['priority'] . "</td>\n";
+  echo '<td>' . $list[$id]['due'] . "</td>\n";
+  echo '<td>';
+  if ($list[$id]['complete']) {
+    echo 'Yes';
+  } else {
+    echo 'No';
+  }
+  echo "</td>\n";
+  echo '</tr>';
+}
+echo $list[0]['title'];
+
+echo '</table>';
+?>
